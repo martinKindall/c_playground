3bb0e0aac764956d491d5c524e9c07a0e12618c8
Martin Cornejo <martin.cornejo@mrge.com>
2023-01-11 00:32:46 +0100

    Using syscalls to open and read files.

---
+++ openfile.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define SIZE 8192
+
+int main() {
+    ssize_t n;
+    char buf[SIZE];
+
+    int fd = open("./somedata.txt", O_RDONLY);
+
+    while ((n= read(fd, buf, SIZE))>0) {
+        printf("Chars read: %ld \n", n);
+        printf("%.*s\n", (int)n, buf);
+    }
+
+    return 0;
+}
